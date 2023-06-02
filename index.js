3afd264e5268cdb542ac15fc86ca431135f3a3dd
Adonis979 <119124735+Adonis979@users.noreply.github.com>
2023-06-03 00:11:37 +0200

    Ready for production

--- index.js
+++ index.js
@@ -23,12 +23,11 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 const corsOptions = {
-  origin: "http://localhost:3000", // Replace with the actual URL of your Next.js application
+  origin: "https://grerezat.vercel.app",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: false,
   optionsSuccessStatus: 204,
 };
-
 app.use(cors(corsOptions));
 app.use("/uploads", express.static("uploads"));
 
@@ -36,6 +35,8 @@ app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/product", productRoute);
 
-app.listen(5000, () => {
-  console.log("backend server runing on http://localhost:5000");
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`backend server running on http://localhost:${port}`);
 });
