1be2b9c6206b10df8f3ddcba8a2e4d4164928845
Adonis979 <119124735+Adonis979@users.noreply.github.com>
2023-06-11 20:24:21 +0200

    Seperated the User into two schemas and fixed some problems regarding adding product with multiple images

--- routes/products.js
+++ routes/products.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const auth = require("../middleware/auth");
 const { validate, Product } = require("../models/Product");
 const Joi = require("joi");
+const { UserType } = require("../models/UserType");
 
 router.post("/add-product", auth, async (req, res) => {
   const {
@@ -21,20 +22,26 @@ router.post("/add-product", auth, async (req, res) => {
     return res.status(400).json({ message: result.error.details[0].message });
   }
 
-  const newProduct = new Product({
-    title,
-    description,
-    peopleCategory,
-    clothesCategory,
-    size,
-    color,
-    price,
-    currency,
-    photos,
-    date: new Date(),
-    publisher: req.user._id,
-  });
+  const userId = req.user._id;
+
   try {
+    const publisherType = await UserType.findOne({ user: userId });
+
+    const newProduct = new Product({
+      title,
+      description,
+      peopleCategory,
+      clothesCategory,
+      size,
+      color,
+      price,
+      currency,
+      photos,
+      date: new Date(),
+      publisher: req.user._id,
+      publisherType: publisherType._id,
+    });
+
     await Product.create(newProduct);
     res.status(200).json({ message: "Product added succesfully" });
   } catch (error) {
@@ -44,10 +51,9 @@ router.post("/add-product", auth, async (req, res) => {
 
 router.get("/", async (req, res) => {
   try {
-    const allProducts = await Product.find().populate(
-      "publisher",
-      "username email phoneNumber type profilePicture -_id"
-    );
+    const allProducts = await Product.find()
+      .populate("publisher", "-password -_id -__v")
+      .populate("publisherType", "-isAdmin -_id -__v -user -date");
     res.status(200).json(allProducts);
   } catch (error) {
     res.status(500).json({ message: error });
@@ -59,12 +65,11 @@ router.get("/:id", async (req, res) => {
   const schema = Joi.required();
   const result = schema.validate(id);
   if (result.error) {
-    if (result.error) {
-      return res.status(400).json({ message: result.error.details[0].message });
-    }
+    return res.status(400).json({ message: result.error.details[0].message });
   }
   try {
-    const product = await Product.findById(id);
+    ``;
+    const product = await Product.findById(id).select("-_id");
     res.status(200).json({ product });
   } catch (error) {
     res.status(404).json({ message: "Product with this id not found" });
@@ -92,14 +97,6 @@ router.get("/user/products", auth, async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: "Something went wrong" });
   }
-});
-
-router.get("/asd/asd", async (req, res) => {
-  res
-    .status(200)
-    .json({
-      message: "Po punon pa db hahhahaha tash edhe me dbbbbb let's gooo",
-    });
 });
 
 module.exports = router;
