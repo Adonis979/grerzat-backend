4c930ceec8ec116bca10bc8f1bc1951e7f6cc6e6
Adonis979 <119124735+Adonis979@users.noreply.github.com>
2023-06-18 00:07:12 +0200

    Changed the way i did the UserType and improved the code overall

--- models/UserType.js
+++ models/UserType.js
@@ -3,22 +3,16 @@ const mongoose = require("mongoose");
 const UserType = mongoose.model(
   "UserType",
   new mongoose.Schema({
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
     type: {
       type: String,
       enum: ["business", "normal"],
       required: true,
-      default: "normal",
     },
     isVerified: { type: String, default: "0" },
     isAdmin: {
       type: Boolean,
       default: false,
     },
-
     date: { type: Date, default: new Date() },
   })
 );
